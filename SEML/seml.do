be23b6feecc45b636bdab1070c7355253cac8180
Thorsten Doherr <doherr@zew.de>
2025-07-31 14:51:10 +0200

    Add files via upload

---
+++ SEML/seml.do
@@ -0,0 +1,322 @@
+/*
+do seml.do
+Trains a neural network based on meta information and scrutinized training samples to identify false positives in 
+SearchEngine results. Several network profiles will be trained and the best in regard of the retained sub-sample 
+will be used to predict false postitives in the meta data. The retention represents the out-of-sample prediction to
+prevent overfitting. 
+
+Requirements:
+The script will need the brain package for STATA. It can be downloaded from the ssc repository with:
+ssc install brain
+The most recent version of the brain module can also be found in the brain sub-directory of the SEML folder of the
+SearchEngine GitHub package. Copy its contents into the current working directory or into the external STATA ado folder.
+
+Input:
+meta.txt - full meta data export of the SearchEngine result table.
+*sample*.txt - scrutinized samples in ExtendedExport format, i.e. sample1.txt, export_sample.txt
+               Multiple sample files matching the template *sample*.txt will be merged.
+			   If there is no sample file, the skript will only conduct the prediction based on an already trained
+			   network in seml.brn.
+All txt files have to be tab-delimited.
+
+Output:
+meta.dta - a copy of the meta data complemented by additional columns based on the raw meta data.
+sample.dta - assigns the essential "equal" variable to every candidate based on the sample files and default setting.
+training.dta - training data: canidate assignment (searched, found, equal), retention indicator, and the meta data.
+seml.brn.log - training output of the confusion matrices if applicable.
+seml.brn - neural network save file using the brain format.
+seml.dta - prediction file (contains reference to sample data if applicable)
+seml.txt - tab delimited prediction file for convenience
+
+Labeling:
+- Read the manual about efficient labeling.
+- The "equal" variable has to be 1 (match) or 9 (non-match) in sample files (zeroes are considered missings).
+- The data is separated into candidate blocks consisting of a header with the search term followed by candidates.
+- A value in a candidate block header defines the default value for the block (reduces typing).
+- The default value in the canidate block header is used for all missings and zeroes within a block.
+- You can define a global default value for the candidate block header in the settings (see below). 
+
+Script schedule:
+If the file seml.brn does not exists, the script will start with the training based on the meta and the sample data.
+If the seml.brn file is created or already existing, it will commence with the prediction.
+It will always try to use the dta files first but will compile them from the txt files if necessary.
+To retrain the network with different settings: delete the seml.brn file
+To retrain the network with different retention: additionally delete the training.dta file
+To retrain the network after changes to the sample file(s): additionally delete sample.dta
+To retrain the network after changes to meta.txt: additionally delete meta.dta
+
+Settings:
+default - global default if "equal" assignment in the candidate block header is missing (default 0)
+retention - share that will not be used for training but for out-of-sampe prediction (default 0.1)
+verbose - 0 = mute, 1 = show blocked iterations, 2 = show all iterations
+hidden - list of neural network hidden layer layouts competing for best out-of-sample accuracy
+balance - 1 = balancing of true and false positives (default), 0 = keep distribution of true/false positives
+epochs - maximum number of training iterations (will not be exhausted in case of plateau, default 5000).
+eta - initial learining rate (default 0.1)
+batch - batch size for training (larger than 1 will activate MP on Windows, default 8)
+*/
+global default = 0 // default for equal: 0 = keep missing, 1 = missing is true positive, 9 = missing is false positive
+global retention = 0.1 // 0.1 will retain 10% of the training data for out-of-sample simulation
+global verbose = 0 // 0 = silent, 1 = noisy, 2 = loud
+global hidden = "[0] [25] [50] [100] [25,25] [50,50] [100,100]" // remove layouts when in a hurry
+global balanced = 1 // 1 to balance false & true positives (default), 0 to keep distribution of true/false positives
+global epochs = 5000 // maximum number of iterations (will not be exhausted in case of plateau)
+global eta = 0.1 // initial learning rate 
+global batch = 8 // mini batch size (larger than 1 will activate MP on Windows)
+
+clear all
+set more off
+
+cap program drop prepare_sample
+program define prepare_sample
+	tempfile sample
+	di as text "checking " as result "sample.dta"
+	cap confirm file sample.dta	
+	if _rc != 0 {
+		di as text "reading *sample*.txt template"
+		local samples : dir "." files "*sample*.txt"
+		foreach f in `samples' {
+			di as text "importing " as result `"`f'"'
+			qui import delimited `"`f'"', enc("latin1") clear
+			keep searched found equal
+			qui drop if searched == . | searched == 0
+			cap destring equal, force replace
+			cap append using `sample'
+			qui save `sample', replace
+		}
+		// removing duplicates in case of multiple sample files with overlap
+		qui replace found = 0 if found == .
+		qui gen long pos = _n
+		sort searched found pos
+		qui drop if searched == searched[_n+1] & found == found[_n+1]
+		sort searched pos
+		drop pos
+		qui replace found = . if found == 0
+		// implementing block and global defaults
+		qui replace equal = $default if found == . & (equal == . | equal == 0)
+		qui egen byte default = max(equal * (found == .)), by(searched)
+		qui replace equal = default if equal == . | equal == 0
+		drop default
+		qui drop if found == .
+		qui replace equal = 1 if equal >= 1 & equal <= 5
+		qui replace equal = 9 if equal > 5 & equal <= 9
+		qui drop if equal != 1 & equal != 9
+		qui replace equal = 0 if equal == 9
+		di as text "saving " as result "sample.dta"
+		qui save sample, replace
+		clear
+	}
+	qui des using sample
+	di as text "rows " as result r(N) as text ", cols " as result r(k)
+end
+
+cap program drop prepare_meta
+program define prepare_meta
+	di as text "checking " as result "meta.dta"
+	cap confirm file meta.dta
+	if _rc != 0 {
+		di as text "importing " as result "meta.txt"
+		qui import delimited meta.txt, enc("latin1") clear
+		sort searched found
+		cap foreach v of varlist csf* {
+			egen `v'sd = sd(`v'), by(searched)
+			qui replace `v'sd = 0 if `v'sd == .
+		}
+		cap foreach v of varlist cfs* {
+			egen `v'sd = sd(`v'), by(searched)
+			qui replace `v'sd = 0 if `v'sd == .
+		}
+		cap gen float cntln = ln(cnt)
+		di as text "saving " as result "meta.dta"
+		qui save meta, replace
+		clear
+	}
+	qui des using meta
+	di as text "rows " as result r(N) as text ", cols " as result r(k)
+end
+
+cap program drop compose_training
+program define compose_training
+	tempname frame
+	di as text "checking " as result "training.dta"
+	cap use training, clear
+	if _rc != 0 {
+		prepare_meta
+		prepare_sample
+		qui use sample, clear
+		qui gen byte retention = uniform() <= $retention
+		order searched found retention equal
+		qui merge n:1 searched found using meta, keep(master match)
+		qui count if _merge == 1
+		if r(N) > 0 {
+			di in red "sample records not found in meta: " as result r(N)
+			error 999
+		}
+		drop _merge
+		qui des, fullnames varlist
+		if regexm(r(varlist),".+ equal[ ]+(.+)") {
+			local meta = regexs(1)
+		}
+		local drop = ""
+		foreach v of varlist `meta' {
+			qui sum `v' if retention
+			if r(max) == r(min) {
+				local drop = "`drop' `v'"
+			}
+		}
+		local drop = trim("`drop'")
+		if "`drop'" != "" {
+			drop `drop'
+			di as text "columns without variation in training data:"
+			di as result "`drop'"
+		}
+		di as text "saving " as result "training.dta"
+		qui save training, replace
+ 	}
+	qui des
+	di as text "rows " as result r(N) as text ", cols " as result r(k)
+	di as text "training and retention" _continue
+	frame put retention equal, into(`frame')
+	frame `frame': qui replace equal = 9 if equal == 0
+	frame `frame': tab equal retention
+	frame drop `frame'
+end
+
+cap program drop training
+program define training
+	tempfile brain
+	qui des, fullnames varlist
+	if regexm(r(varlist),".+ equal[ ]+(.+)") {
+		local meta = regexs(1)
+	}
+	if "`meta'" == "" {
+		di as error `"no meta variables defined after "equal""'
+		error 999
+	}
+	local weight = ""
+	local balanced = ""
+	if $balanced {
+		qui sum equal if retention == 0
+		if r(mean) > 0.5 {
+			qui gen double weight = r(mean)/(1-r(mean)) if equal == 0 & retention == 0
+			qui replace weight = 1 if equal == 1 & retention == 0
+		}
+		else {
+			qui gen double weight = (1-r(mean)) / r(mean) if equal == 1 & retention == 0
+			qui replace weight = 1 if equal == 0 & retention == 0
+		}
+		local weight = " [pweight=weight]"
+		local balanced = " (balanced)"
+	}
+	local hidden = subinstr("$hidden"," ","",.)
+	local hidden = subinstr(subinstr("`hidden'","],[","][",.),"]["," ",.)
+	local hidden = subinstr(subinstr("`hidden'","[","",.),"]","",.)
+	local model = 0
+	local best_acc = 0
+	local best_model = 0
+	local best_layout = ""
+	cap log close training
+	qui log using seml.brn.log, name(training) text nomsg replace
+	log off training
+	foreach layers in `hidden' {
+		if "`layers'" == "0" {
+			local layers = ""
+		}
+		local model = `model'+1
+		local layout = wordcount("`meta'")
+		local layout = "`layout' `layers' 1"
+		local layout = subinstr(subinstr(subinstr("`layout'","  "," ",.)," ","x",.),",","x",.)
+		log on training
+		di as text "model " as result `model' as text ": " as result "`layout'`balanced'"
+		log off training
+		qui brain define if retention == 0, input(`meta') output(equal) hidden(`layers') spread(0.01)
+		local eta = $eta
+		local stop = `eta'/(2^5)
+		local run = 1
+		local best = 0
+		local sp = ""
+		local qui = "qui"
+		if "`layers'" == "" {
+			local sp = "sp"
+		}
+		if $verbose == 2 {
+			local qui = ""
+		}
+		while `eta' >= `stop' & `run' <= $epochs {
+			if $verbose {
+				local epoch = `run'-1+100
+				local epoch = "`run'-`epoch'"
+				di as text "epoch " as result "`epoch'" as text ", eta " as result `eta' as text ": " _continue
+			}
+			`qui' brain train`weight' if retention == 0, eta(`eta') iter(100) report(10) batch($batch) best `sp'
+			if r(iter) == 0 {
+				local eta = `eta'/2
+			}
+			else {
+				local best = `run'-1+r(iter)
+			}
+			if $verbose {
+				di as text "loss " as result r(err) as text ", best " as result `best'
+			}
+			local run = `run' + 100
+		}
+		log on training
+		brain fit equal if retention, `sp'
+		local acc = r(accuracy)
+		log off training
+		if `acc' > `best_acc' {
+			local best_acc = `acc'
+			local best_model = `model'
+			local best_layout = "`layout'"
+			brain save `brain'
+		}
+	}
+	qui brain load `brain'
+	log on training
+	di as text "best model " as result `best_model' as text ": " as result "`best_layout'"
+	brain fit equal if retention
+	log close training
+	di as text "saving " as result "seml.brn"
+	brain save seml.brn
+end
+
+cap program drop prediction
+program define prediction
+	prepare_meta
+	qui use meta, clear
+	di as text "predicting..."
+	qui brain think brain
+	keep searched found brain
+	qui gen byte _equal = cond(brain > 0.5,1,9)
+	cap merge 1:1 searched found using sample, keep(master match) update replace
+	if _rc == 0 {
+		di as text "attaching " as result "sample.dta" _continue
+		drop _merge
+		qui replace equal = 9 if equal == 0
+		rename equal sample
+		rename _equal equal
+		tab equal sample
+	}
+	cap rename _equal equal
+	order searched found brain equal
+	format brain %8.6f
+	di as text "saving " as result "seml.dta" as text " and " as result "seml.txt"
+	qui save seml, replace
+	qui export delimited seml.txt, delim(tab) noquote datafmt replace
+end
+
+cap program drop main
+program define main
+	cap brain load seml.brn
+	if _rc != 0 {
+		di as result "TRAINING"
+		compose_training
+		training
+	}
+	di as result "PREDICTION"
+	prediction
+	display as result "done."
+	qui // STATA may have a problem with "display" as last command in sub-programs
+end
+
+main
---
+++ SEML/seml.py
@@ -0,0 +1,313 @@
+"""
+python seml.py
+Trains a neural network based on meta information and scrutinized training samples to identify false positives in 
+SearchEngine results. Several network profiles will be trained and the best in regard of the retained sub-sample 
+will be used to predict false postitives in the meta data. The retention represents the out-of-sample prediction to
+prevent overfitting. 
+
+Requirements:
+The script will need Python 3.x. It will try to automatically download all necessary python packages if not already
+installed. Following packages are required: tensorflow (includes numpy and keras), scikit-learn and pandas.
+If you want to have more control over the installation you can install them manually with:
+python -m pip install <package-name>
+Tensorflow may throw exceptions if specific system requirements are not fulfilled. On Windows systems, many of these are
+related to a missing or outdated "Microsoft Visual C++ Redistributable", which can be downloaded for free directly from
+Microsoft (just google it).
+
+Input:
+meta.txt - full meta data export of the SearchEngine result table.
+*sample*.txt - scrutinized samples in ExtendedExport format, i.e. sample1.txt, export_sample.txt
+               Multiple sample files matching the template *sample*.txt will be merged.
+			   If there is no sample file, the script will only conduct the prediction based on an already trained
+			   network in seml.brain.
+
+Output:
+meta.csv - a copy of the meta data complemented by additional columns based on the raw meta data.
+sample.csv - assigns the essential "equal" variable to every candidate based on the sample files and default setting.
+training.csv - training data: canidate assignment (searched, found, equal), retention indicator, and the meta data.
+seml.brain.log - training output of the confusion matrices if applicable.
+seml.brain - neural network model save file (zipped keras model + variable names used for training).
+seml.csv - prediction file (contains reference to sample data if applicable)
+
+All txt files have to be tab-delimited.
+All csv files are comma-separated.
+
+Labeling:
+- Read the manual about efficient labeling.
+- The "equal" variable has to be 1 (match) or 9 (non-match) in sample files (zeroes are considered missings).
+- The data is separated into candidate blocks consisting of a header with the search term followed by candidates.
+- A value in a candidate block header defines the default value for the block (reduces typing).
+- The default value in the canidate block header is used for all missings and zeroes within a block.
+- You can define a global default value for the candidate block header in the settings (see below). 
+
+Script schedule:
+If the file seml.brain does not exists, the script will start with the training based on the meta and the sample data.
+If the seml.brain file is created or already existing, it will commence with the prediction.
+It will always try to use the csv files first but will compile them from the txt files if necessary.
+To retrain the network with different settings: delete the seml.brain file
+To retrain the network with different retention: additionally delete the training.csv file
+To retrain the network after changes to the sample file(s): additionally delete sample.csv
+To retrain the network after changes to meta.txt: additionally delete meta.csv
+
+Settings:
+default - global default if "equal" assignment in the candidate block header is missing (default 0)
+retention - share that will not be used for training but for out-of-sampe prediction (default 0.1)
+verbose - 0 = mute, 1 = show iterations, 2 = progress bar per iteration
+hidden - list of neural network hidden layer layouts competing for best out-of-sample accuracy
+balance - True = balancing of true and false positives (default), False = keep original distribution
+epochs - number of training iterations (default 500)
+batch - batch size for training (default 8)
+"""
+default = 0 # default for equal: 0 = keep missing, 1 = missing is true positive, 9 = missing is false positive
+retention = 0.1 # 0.1 will retain 10% of the training data for out-of-sample simulation 
+verbose = 0 # 0 = silent, 1 = noisy, 2 = loud
+hidden = [[0], [25], [50], [100], [25,25], [50,50], [100,100]] # remove layouts when in a hurry
+balanced = True # True for balancing false & true positives, False otherwise
+epochs = 500 # reduce when in a hurry, increase for diminishing returns
+batch = 8 # lower is slower with very slight gains in efficiency
+
+import os 
+import sys
+import glob
+import importlib
+import subprocess
+from zipfile import ZipFile
+
+def install_package(package_name, install_name=None):
+    try:
+        importlib.import_module(package_name)
+    except ImportError:
+        print(f"INSTALLING {package_name}...")
+        if not install_name:
+            install_name = package_name
+        subprocess.check_call([sys.executable, "-m", "pip", "install", install_name])
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+install_package('tensorflow')
+install_package('sklearn', 'scikit-learn')
+install_package('pandas')
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.utils import class_weight
+from tensorflow.keras import models, Sequential
+from tensorflow.keras.layers import Normalization, Dense
+
+def read_sample():
+    print('checking sample.csv')
+    sample = None
+    try:
+        sample = pd.read_csv('sample.csv', delimiter=',', encoding='latin1', index_col=0) 
+    except:
+        print('reading *sample*.txt template')
+        files = glob.glob('*sample*.txt')
+        for f in files:
+            print(f)
+            if isinstance(sample, pd.DataFrame):
+                sample = sample.merge(read_a_sample(f), how='outer', on=['searched','found'], suffixes=(None, '_u'), indicator=True)
+                overlap = sample.loc[sample['_merge'] == 'both', ['equal','equal_u']]
+                print(f'overlap {len(overlap)}')
+                overlap = overlap.loc[overlap['equal'] != overlap['equal_u']]
+                if len(overlap):
+                    print(f'conflicts {len(overlap)}')
+                sample['equal'] = sample['equal'].fillna(sample['equal_u']).astype('int')
+                sample = sample[['searched','found','equal']]
+            else:
+                sample = read_a_sample(f)
+        sample = sample[['searched','found','equal']]
+        sample.to_csv('sample.csv')
+    print(f'{sample.shape[0]} rows, {sample.shape[1]} cols')
+    return sample
+        
+def read_a_sample(file):
+    sample = pd.read_csv(file, delimiter='\t', encoding='latin1', index_col=False)
+    sample.columns = sample.columns.str.lower()
+    sample = sample[['searched','found','equal']].replace(0, np.nan)
+    sample = sample.loc[sample['searched'].notna()]
+    equal = sample.loc[sample['found'].isna(),['searched','equal']]
+    equal['equal'] = equal['equal'].fillna(default)
+    sample.dropna(subset=['found'], inplace=True)
+    sample = sample.merge(equal, how='left', on='searched', suffixes=(None, '_u'))
+    sample['equal'] = sample['equal'].fillna(sample['equal_u'])
+    sample = sample.drop(columns=['equal_u']).dropna().astype('int')
+    sample['equal'] = sample['equal'].apply(lambda x : 1 if x > 0 and x <= 5 else 0 if x > 5 and x <= 9 else 9)
+    sample.drop(sample[sample['equal'] == 9].index, inplace=True)
+    return sample
+    
+def read_meta():
+    print('checking meta.csv')
+    meta = None
+    try:
+        meta = pd.read_csv('meta.csv', delimiter=',', encoding='latin1', index_col=0) 
+    except:
+        print('reading meta.txt')
+        meta = pd.read_csv('meta.txt', delimiter='\t', encoding='latin1', index_col=False)
+        meta.columns = meta.columns.str.lower()
+        meta['searched'] = meta['searched'].astype('int')
+        meta['found'] = meta['found'].astype('int')
+        meta['cntln'] = np.log(meta['cnt'])
+        # standard deviations of string comparisons within "searched" group
+        for c in meta.columns:
+            if (c.startswith('csf') or c.startswith('cfs')) and c[3:].isdigit():
+                meta[c+'sd'] = meta.groupby('searched')[c].transform('std').fillna(0)
+        meta.to_csv('meta.csv')
+    print(f'{meta.shape[0]} rows, {meta.shape[1]} cols')
+    return meta
+
+def compose_training():
+    print('checking training.csv')
+    try:
+        train = pd.read_csv('training.csv', delimiter=',', encoding='latin1', index_col=0)
+    except:
+        meta = read_meta()
+        sample = read_sample()
+        sample['retention'] = np.random.uniform(size=len(sample))
+        sample['retention'] = np.where(sample['retention'] <= retention, 1, 0).astype('int')
+        sample = sample[['searched','found','retention','equal']]
+        train = sample.merge(meta, how='inner', on=['searched','found'])
+        if len(train) != len(sample):
+            raise ValueError(f'sample records not found in meta: {len(sample)-len(train)}')
+        sample = None # clear memory
+        meta = None # clear memory
+        # no variation within train data
+        min = train.loc[train['retention'] == 0, train.columns.difference(['searched','found','equal','retention'])].min()
+        max = train.loc[train['retention'] == 0, train.columns.difference(['searched','found','equal','retention'])].max()
+        drop = min[min == max]
+        if drop.shape[0]:
+            train.drop(columns=drop.index, inplace=True)
+            print(f'columns without variation in training data:\n{", ".join(drop.index)}')
+        print('saving training.csv')
+        train.to_csv('training.csv')
+    print(f'{train.shape[0]} rows, {train.shape[1]} cols')
+    print('training and retention')
+    tab = train[['retention','equal']].groupby(['retention','equal']).value_counts().to_frame().reset_index()
+    tab['equal'] = tab['equal'].replace(0, 9)
+    print(tab.to_string(index=False))
+    return train
+
+def training(train):
+    print('training...')
+    x_train = train.loc[train['retention']==0,'identity':].reset_index(drop=True)
+    y_train = train.loc[train['retention']==0,'equal'].reset_index(drop=True)
+    x_test = train.loc[train['retention']==1,'identity':].reset_index(drop=True)
+    y_test = train.loc[train['retention']==1,'equal'].reset_index(drop=True)
+    normalizer = Normalization(axis=-1)
+    normalizer.adapt(x_train.values)
+    if balanced:
+        class_weights = dict(enumerate(class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)))
+    else:
+        class_weights = {0: np.float64(1), 1: np.float64(1)}
+    best_acc = 0
+    best_layer = 0
+    best_model = None
+    best_cm = None
+    with open('seml.brain.log', 'w') as log:
+        for num, layers in enumerate(hidden):
+            layers = [l for l in layers if l > 0]
+            line = f"model {num+1}: layers {'x'.join([str(l) for l in [len(x_train.columns)]+layers+[1]])}, epochs {epochs}, batch {batch}{', balanced' if balanced else ''}" 
+            print(line)
+            log.write(line+'\n')
+            model = Sequential()
+            model.add(normalizer)
+            for layer in layers:
+                model.add(Dense(units=layer, activation='relu'))
+            model.add(Dense(units=1, activation='sigmoid'))
+            model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+            model.fit(x_train, y_train, class_weight=class_weights, epochs=epochs, batch_size=batch, verbose=verbose)
+            y_pred = (model.predict(x_test, verbose=0) > 0.5).astype('int')
+            cm = confusion_matrix(y_test, y_pred)
+            acc, tab = confuse(cm)
+            print(tab)
+            log.write(tab+'\n')
+            if acc > best_acc:
+                best_acc = acc
+                best_layer = num
+                best_model = model
+                best_cm = cm
+        line = f"best model {best_layer+1}: layers {'x'.join([str(l) for l in [len(x_train.columns)]+hidden[best_layer]+[1]])}, epochs {epochs}, batch {batch}{', balanced' if balanced else ''}"
+        acc, tab = confuse(best_cm)
+        print(line)
+        print(tab)
+        log.write(line+'\n')
+        log.write(tab+'\n')
+    print('saving seml.brain')
+    names = list(x_train.columns)
+    save_brain(model, names)
+    return model, names
+
+def prediction(model, names):
+    meta = read_meta()
+    meta.drop(meta.columns.difference(['searched','found']+names), axis=1, inplace=True)
+    meta.columns = ['searched','found']+names
+    seml = meta.loc[:,['searched','found']]  # separate keys from data
+    meta.drop(['searched','found'], axis=1, inplace=True)  # pure data without keys
+    print('predicting...')
+    print(f'{meta.shape[0]} rows, {meta.shape[1]} cols')
+    seml.loc[:,['brain']] = model.predict(meta.values, verbose=verbose)
+    seml['equal'] = np.where(seml['brain'] > 0.5, 1, 9).astype('int')
+    tab = seml[['equal']].groupby(['equal']).value_counts()
+    tab['sum'] = tab.sum()
+    tab = tab.to_frame()
+    tab['share'] = tab['count']/tab.loc['sum','count']*100
+    print(tab.reset_index().to_string(index=False))
+    try:
+        sample = pd.read_csv('sample.csv', delimiter=',', encoding='latin1', index_col=0) 
+    except:
+        print('no sample.csv detected')
+    else:
+        print('attaching sample.csv')
+        seml = seml.merge(sample, how='left', on=['searched','found'], suffixes=('','_s'))
+        seml['sample'] = np.where(seml['equal_s'] == 0, 9, seml['equal_s'])
+        seml['sample'] = seml['sample'].fillna(0).astype('int')
+        seml.drop(seml.columns.difference(['searched','found','brain','equal','sample']), axis=1, inplace=True)
+        tab = seml.loc[seml['sample'] != 0, ['equal','sample']].groupby(['equal','sample']).value_counts().to_frame()
+        print(tab.reset_index().to_string(index=False))
+    print('saving seml.csv')
+    seml.to_csv('seml.csv')
+    
+def confuse(cm):
+    tn, fp = cm[0]
+    fn, tp = cm[1]
+    acc = (tp+tn)/(tp+tn+fp+fn)
+    tab = f'acc {acc*100:6.2f}            true        false\n'
+    tab += f'positive      {tp:12} {fp:12}\n'
+    tab += f'negative      {tn:12} {fn:12}\n'
+    tab += f'recall              {tp/(tp+fn)*100:6.2f}       {tn/(tn+fp)*100:6.2f}\n'
+    tab += f'precision           {tp/(tp+fp)*100:6.2f}       {tn/(tn+fn)*100:6.2f}'
+    return (acc, tab) 
+    
+def save_brain(model, names):
+    model.save("seml.keras")
+    with open("seml.names", mode="w") as f:
+        f.write(",".join(names))
+    with ZipFile("seml.brain", mode="w") as f:
+        f.write("seml.keras")
+        f.write("seml.names")
+    os.remove("seml.keras")
+    os.remove("seml.names")
+   
+def load_brain():
+    with ZipFile("seml.brain", mode="r") as f:
+        f.extract("seml.keras")
+        f.extract("seml.names")
+    model = models.load_model("seml.keras")
+    with open("seml.names", mode="r") as f:
+        names = f.readline().strip().split(',')
+    os.remove("seml.keras")
+    os.remove("seml.names")
+    return (model, names)
+
+def main(argv):
+    try:
+        model, names = load_brain()
+    except FileNotFoundError:
+        print('TRAINING')
+        train = compose_training()
+        model, names = training(train)
+        train = None
+    print('PREDICTION')
+    prediction(model, names)
+    print('done.')
+    
+if __name__ == "__main__": sys.exit(main(sys.argv))
